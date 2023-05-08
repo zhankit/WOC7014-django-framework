812d6b3ec2e8e80fa562c29b35c310601baca1ef
Ding <zhankit1209@gmail.com>
2023-05-08 18:42:54 +0800

    add read me

--- README.md
+++
@@ -1,8 +0,0 @@
-# WOC7014-django-framework
-Framework-based software design and development
-
-## Pre-requiste
-- Docker
-
-# Startup
-`docker-compose up --build -d`
--- app/HelloWorlds/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HelloworldsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'HelloWorlds'
